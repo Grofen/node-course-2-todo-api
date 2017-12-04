1347e8ebffb3fc93b53b563391de0eedecfc5e06
Grofen <mohammadghorab2@gmail.com>
2017-12-04 17:01:56 +0100

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -101,7 +101,7 @@ app.post('/users', (req, res) => {
    user.save().then(() => {
      return user.generateAuthToken();
    }).then((token) => {
-     res.header('x-auth', token).send(user)
+     res.header('x-auth', token).send(user);
    }).catch((err) => {
      res.status(400).send(err);
    });
@@ -109,6 +109,18 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+ var body = _.pick(req.body, ['email', 'password']);
+
+ User.findByCredentials(body.email, body.password).then((user) => {
+   user.generateAuthToken().then((token) => {
+     res.header('x-auth', token).send(user);
+   });
+ }).catch((err) => {
+   res.status(400).send();
+ });
 });
 
 app.listen(port, () => {
