0e0e4c8291f1773bed74754f7e8011edde7fa65f
Grofen <mohammadghorab2@gmail.com>
2017-12-11 00:08:03 +0100

    Associate users with todos

--- server/test/seed/seed.js
+++ server/test/seed/seed.js
@@ -17,17 +17,23 @@ const users = [{
 }, {
   _id: userTowId,
   email: 'omar@example.com',
-  password: 'userTowPass'
+  password: 'userTowPass',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: userTowId, access: 'auth'}, 'abc123').toString()
+  }]
 }];
 
 const todos = [{
   _id: new ObjectID(),
-  text: 'First test todo'
+  text: 'First test todo',
+  _creator: userOneId
 }, {
   _id: new ObjectID(),
   text: 'Second test todo',
   completed: true,
-  completedAt: 333
+  completedAt: 333,
+  _creator: userTowId
 }];
 
 const populateTodos = (done) => {
